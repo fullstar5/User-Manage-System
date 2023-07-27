ab61c0393572f720040bacd1432e047133cfa35d
fullstar5 <1518709701@qq.com>
2023-07-27 20:25:40 +1000

    update 27/07/2023
    solved cross origin problem by changing front-end request link to correct link to back-end

--- back_end/user-center/src/main/java/com/star/usercenter/exception/GlobalExceptionHandler.java
+++ back_end/user-center/src/main/java/com/star/usercenter/exception/GlobalExceptionHandler.java
@@ -12,14 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public BaseResponse businessExceptionHandler(BusinessException e){
-        log.error("BusinessException" + e.getMessage(), e);
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
         return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
     }
 
+
     @ExceptionHandler(RuntimeException.class)
-    public BaseResponse runtimeExceptionHandler(RuntimeException e){
-        log.error("RunTimeException", e);
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
     }
 
